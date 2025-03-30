5e24c395134325664bcf08bffb6e40d3fbda5e5d
btessera <btessera@gmu.edu>
2025-03-29 23:44:44 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
 
 // Routes
 const podcastRoutes = require('./routes/podcasts');
 app.use('/api/podcasts', podcastRoutes);
 
-// Root Route
+// Define a route for "/"
 app.get('/', (req, res) => {
   res.send('Welcome to PodAura Backend!');
 });
 
-// Start Server
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
